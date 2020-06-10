cb47902359145288c1b50ed85e0c85144de7263d
oriolsubirana <subi.cat@gmail.com>
2020-06-10 11:58:50 +0200

    mower application

---
+++ src/main/java/com/oriol/mower/MowerApplication.java
@@ -0,0 +1,13 @@
+package com.oriol.mower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MowerApplication.class, args);
+	}
+
+}
